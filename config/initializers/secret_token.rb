50d90d57161d0fddc4d1cbf9b3f317ce3842109a
Bash Hammouri <eng.bash85@gmail.com>
2013-02-24 20:10:37 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleDemo::Application.config.secret_token = '3049d2a1d88e9da296b37cf6a885440cd909203d6e32bbed900d31484c4b78f2a4321899f49cb05846d73acb0fb9edca6d3b30cfc5cdb323dfadaff1a0b490cf'
